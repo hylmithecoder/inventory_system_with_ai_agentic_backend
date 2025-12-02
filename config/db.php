475f0c5bb01db8a567ad2b874712699306137c4a
hylmithecoder <hylmimuhammadfiarymahdi@gmail.com>
2025-12-02 19:22:45 +0700

    Update dockerfile

--- config/db.php
+++ config/db.php
@@ -1,2 +1,7 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "hackathon_imphnen") or die("Connection Failed");
+$host = getenv('DB_HOST') ?: 'localhost'; 
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: '';
+$db   = getenv('DB_NAME') ?: 'hackathon_imphnen';
+
+$conn = mysqli_connect($host, $user, $pass, $db) or die("Connection Failed");
