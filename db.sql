6b87099aa3d22a39ad36503137688b213022b74a
hylmithecoder <hylmimuhammadfiarymahdi@gmail.com>
2025-12-06 07:10:34 +0700

    update db

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 03, 2025 at 03:59 AM
+-- Generation Time: Dec 05, 2025 at 04:25 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,8 +42,8 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`ID`, `created_at`, `username`, `password`, `isAdmin`, `session_token`, `session_expiry`) VALUES
-(1, '2025-12-01 05:16:46', 'hylmi', '$2a$12$aMkBUAVjtwfI46Us13Jx0.cNcJVa5ri0MaUFWt97U5fi4yupNBlS2', 'YES', 'bb4e3ba010e830fd0d47badcff6129b5619b97b88e5793b7bb2341f59eaace32', '2025-12-03 22:53:45'),
-(2, '2025-12-01 14:49:20', 'test', '$2y$12$Sg50YvG6VnAeNp2dmgoujeP3iOAeqHfSyH9b2GzVsF.kALL81egK2', 'YES', NULL, NULL),
+(1, '2025-12-01 05:16:46', 'hylmi', '$2a$12$aMkBUAVjtwfI46Us13Jx0.cNcJVa5ri0MaUFWt97U5fi4yupNBlS2', 'YES', 'e507a1328ef4026ee0b76045f2c02464cea7c0db19fd1ffb6966783e5427e5f7', '2025-12-06 18:51:32'),
+(2, '2025-12-01 14:49:20', 'test', '$2y$12$Sg50YvG6VnAeNp2dmgoujeP3iOAeqHfSyH9b2GzVsF.kALL81egK2', 'YES', 'd6b2d9dfc680bb58acb796a2797b489c2a244d96156d2161ad6b7532ab4a5314', '2025-12-06 08:57:07'),
 (3, '2025-12-02 01:17:20', 'akuruoaklah', '$2y$12$0/zkk9Vs1OZaQWoeLLFhIuP4aAULm1wyNHfKu7x3j/fmlYyXQSd.K', 'YES', NULL, NULL);
 
 -- --------------------------------------------------------
@@ -66,11 +66,7 @@ CREATE TABLE `history_chat` (
 --
 
 INSERT INTO `history_chat` (`ID`, `created_at`, `request`, `response`, `sql_script`, `created_by`) VALUES
-(3, '2025-12-03 01:40:12', 'Input 10 floridina di table inventory', 'baik saya akan....', 'INSERT INTO inventory (name, stock, price, created_by) VALUES (\'Floridina\', 10, 3000, \'hylmi\')', 'hylmi'),
-(4, '2025-12-03 02:52:12', 'Input 10 floridina di table inventory', 'baik saya akan....', 'INSERT INTO inventory (name, stock, price, created_by) VALUES (\'Floridina\', 10, 3000, \'hylmi\')', 'hylmi'),
-(5, '2025-12-03 02:52:30', 'Input 10 floridina di table inventory', 'baik saya akan....', 'SHOW TABLES', 'hylmi'),
-(6, '2025-12-03 02:58:30', 'Input 10 floridina di table inventory', 'baik saya akan....', 'SELECT * FROM account', 'hylmi'),
-(7, '2025-12-03 02:58:46', 'Input 10 floridina di table inventory', 'baik saya akan....', 'SELECT username FROM account', 'hylmi');
+(47, '2025-12-05 14:41:12', 'coba baca tabel itu dan tambah produk mekanikal keyboard itu 30 lagi', 'Memperbarui kolom \'stock\' untuk produk \'Mechanical Keyboard\' di tabel \'inventory\' dengan menambahkan 30 unit ke stok saat ini.', 'UPDATE inventory SET stock = stock + 30 WHERE name = \'Mechanical Keyboard\';', 'hylmi');
 
 -- --------------------------------------------------------
 
@@ -92,12 +88,14 @@ CREATE TABLE `inventory` (
 --
 
 INSERT INTO `inventory` (`ID`, `created_at`, `name`, `stock`, `price`, `created_by`) VALUES
-(1, '2025-12-01 05:16:01', 'Nasi goreng', 5, 10000.00, 'hylmi'),
-(6, '2025-12-02 00:57:22', 'Mie ayam', 10, 12000.00, 'hylmi'),
-(7, '2025-12-02 01:14:54', 'Nasi goreng kampung', 10, 15000.00, 'hylmi'),
-(10, '2025-12-02 02:58:06', 'Ayam Kentucky', 10, 1000.00, 'hylmi'),
-(23, '2025-12-03 01:40:12', 'Floridina', 10, 3000.00, 'hylmi'),
-(24, '2025-12-03 02:52:12', 'Floridina', 10, 3000.00, 'hylmi');
+(35, '2025-12-05 14:18:45', 'Mechanical Keyboard', 30, 1200000.00, 'hylmi'),
+(36, '2025-12-05 14:18:45', 'Wireless Mouse Logitech', 10, 500000.00, 'hylmi'),
+(42, '2025-12-05 14:18:45', 'Portable Power Bank 20000mAh', 5, 350000.00, 'hylmi'),
+(43, '2025-12-05 14:18:45', 'USB-C Cable 2m', 50, 50000.00, 'hylmi'),
+(58, '2025-12-05 14:18:45', 'Wireless Charger Pad', 0, 400000.00, 'hylmi'),
+(59, '2025-12-05 14:18:45', 'Bluetooth Speaker Portable', 10, 250000.00, 'hylmi'),
+(62, '2025-12-05 14:18:45', 'Printer Laser Mono', 160, 3000000.00, 'hylmi'),
+(67, '2025-12-05 14:18:45', 'Fitness Tracker Xiaomi', 0, 450000.00, 'hylmi');
 
 --
 -- Indexes for dumped tables
@@ -135,13 +133,13 @@ ALTER TABLE `account`
 -- AUTO_INCREMENT for table `history_chat`
 --
 ALTER TABLE `history_chat`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
